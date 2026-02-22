71f01628c3dcb11433d404736a19efa2faf2f4f1
TwiliChaos <accounts+github@ilemni.com>
2026-02-21 18:25:23 -0800

    Mio Purple implementation

--- src/my_mod.cpp
+++ src/my_mod.cpp
@@ -1,36 +1,39 @@
 #include <windows.h>
-#include <stdio.h>
 #include "modding_api.h"
 
-DWORD WINAPI MyModCode(LPVOID lpParam) {
-    // Code from "Slight Breeze" mod
+constexpr uintptr_t isAtiOffset = 0x110FBB8;
+
+DWORD WINAPI MyModCode(LPVOID) {
+    HMODULE hModule = GetModuleHandleA("mio.exe");
+    if (!hModule) {
+        LogMessage("ERROR: Failed to get mio.exe module handle!");
+        return 0;
+    }
+
+    const auto baseAddr = reinterpret_cast<uintptr_t>(hModule);
+    const auto playerIsAtiPtr = reinterpret_cast<void *>(baseAddr + isAtiOffset);
+
     while (true) {
-        Sleep(10);  // Update every 10ms
-        
-        f32x3 loc = GetPlayerLocation(); // Gets the player's location
-        
-        if (loc.x == -1.0f || loc.y == -1.0f) {
+        Sleep(10); // Update every 10ms
+
+        if (const f32x3 loc = GetPlayerLocation(); loc.x == -1.0f || loc.y == -1.0f) {
             continue; // Wait until save is loaded
         }
-        printf("Old Player Position: X=%.2f, Y=%.2f, Z=%.2f     \n", loc.x, loc.y, loc.z);
 
-        loc.x += 0.03;
-        SetPlayerLocation(loc); // Sets the player's location to the new location
-        
-        printf("New Player Position: X=%.2f, Y=%.2f, Z=%.2f     \n", loc.x, loc.y, loc.z);
-        fflush(stdout);
+        WriteMemoryTyped(playerIsAtiPtr, 1);
+        Sleep(1000); // Sleep 1s
     }
     return 0;
 }
 
 extern "C" __declspec(dllexport) void ModInit() {
-    LogMessage("\"My\" Mod mod loaded!");
-    
+    LogMessage("\"Mio Purple\" mod loaded!");
+
     // Start monitoring thread
     CreateThread(nullptr, 0, MyModCode, nullptr, 0, nullptr);
 }
 
-BOOL APIENTRY DllMain(HMODULE hModule, DWORD ul_reason_for_call, LPVOID lpReserved) {
+BOOL APIENTRY DllMain(HMODULE, DWORD, LPVOID) {
     // Mods don't need to do anything in DllMain
     return TRUE;
 }
